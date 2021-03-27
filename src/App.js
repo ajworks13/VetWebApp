5d8ef05462726031bb038f7469182a82719429f0
Aneudy <ilovethe80s90s@yahoo.com>
2021-03-27 10:48:17 -0400

    added some files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+//import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Container from './components/Container';
+import SignUp from './components/pages/SignUp';
+import LogIn from './components/pages/LogIn';
+import HomePage from './components/pages/HomePage'
+import Account from './components/pages/Account'
+import Settings from './components/pages/Settings'
+
+
+//HOT KEYs for productiveness:
+// ALT+SHIFT+ DOWN | UP arrow     to copy line.
+// ALT+Z    for soft wrap
+// SHIFT+TAB     to tab backwards
+//-----------------------------------------------
+
+// Make MainMenu, Account, Settings, HospitalInfo
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          {/* <Route path='/' exact component={Container}/> */}
+          <Route path='/' exact component={SignUp}/>
+          <Route path='/LogIn' component={LogIn}/>
+          <Route path='/HomePage' component={HomePage}/>
+          <Route path='/Account' component={Account}/>
+          <Route path='/Settings' component={Settings}/>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+
+function Container() {
+    return (
+        <>
+            <SignUp />
+            
+        </>
+    )
+}
+
+export default Container;
